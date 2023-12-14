609d86ad9a6bfaa41dc521ee8b719e89fe7fead2
Ayush Manojkumar Lodha <63442869+ayushlodha7@users.noreply.github.com>
2023-12-14 15:05:13 -0500

    Update server.py
    Added the before hand data size

--- python-server/server.py
+++ python-server/server.py
@@ -1,7 +1,22 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response,jsonify
 import struct
 
 app = Flask(__name__)
+
+# Prompt for the data size when the server starts
+try:
+    array_size = int(input("Enter the size for data to send: "))
+except ValueError:
+    print("Invalid input! Using default size 20.")
+    array_size = 20
+
+@app.route('/sent_data_size', methods=['GET'])
+def sent_data_size():
+    try:
+        return Response(str(array_size), mimetype='text/plain')
+    except Exception as e:
+        print(f"Error: {e}")
+        return "Failed to get data size", 500
 
 @app.route('/receive_data', methods=['POST'])
 def receive_data():
@@ -17,10 +32,10 @@ def receive_data():
 
 @app.route('/send_data', methods=['GET'])
 def send_data():
-    array_size = 2000000
-    data_to_send = [float(i) for i in range(array_size)]  # Python's float is equivalent to C's double
+    data_to_send = [float(i) for i in range(array_size)]
     binary_data = struct.pack(f'{array_size}d', *data_to_send)
     return Response(binary_data, mimetype='application/octet-stream')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
